a7577ac6a4d7dc64aaa6ddf304dce6d79ce93971
Nasina7 <38535607+Nasina7@users.noreply.github.com>
2025-08-07 21:31:16 -0400

    Update puyomdtool to v1.0.2

--- build_flags.asm
+++ build_flags.asm
@@ -14,18 +14,3 @@ fFixTitleArleUB = 0
 
 ; Fix a harmless incorrect value in Video_ClearScrollTables
 fFixClearScrollFunc = 0
-
-; Todo: This flag is not set up yet.
-; Implement initialization checks for objects that don't have them.
-; This bug will only cause writes to ROM, but it's still good to have a fix for it.
-fFixObjectInits = 0
-
-; Todo: This flag is not set up yet.  (Need to disassemble sound bank 1 first)
-; Fix a bug(?) where "Warning of Puyo Puyo" will continually increase instrument pitch forever, rather than looping normally.
-fFixWarningOfPuyo = 0
-
-; ---------- Optimization ----------
-
-; Todo: This flag is not set up yet.
-; This flag will remove all unused code and data, as well as any unnecessary padding.
-fOptRemoveUnused = 0
